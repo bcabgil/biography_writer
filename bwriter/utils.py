e7fb2ce35d920c5c49f671f92b2fc3e697d56d04
bcabgil <b.cabgil@gmail.com>
2025-07-20 11:59:12 +0200

    Add verify_path() and add cleaning ofption to read_file()

--- bwriter/utils.py
+++ bwriter/utils.py
@@ -2,14 +2,19 @@
 
 from pathlib import Path
 from typing import List, Union
+
 from utils.logger import Logger
+
 logger = Logger.get_logger()
 
 
-def read_file(filepath: Path) -> str:
-    """Read the content of a file."""
-    with open(filepath, "r") as file:
+def read_file(filepath: Path, clean: bool = True) -> str:
+    """Read the content of a file. If clean is True, it will convert the content to lowercase and strip whitespace."""
+    with open(filepath, "r", encoding="latin-1") as file:
         file_content = file.read()
+    if clean:
+        file_content = file_content.lower().strip()
+    logger.info(f"Reading file: {filepath}")
 
     return file_content
 
@@ -28,3 +33,19 @@ def write_file(filepath: Path, text: Union[str, List[str]]) -> None:
             save_file.writelines(text)
         else:
             save_file.write(text)
+
+
+def verify_path(path: Path) -> None:
+    """Verify if a path exists and is a directory.
+
+    Args:
+        path (Path): The path to verify.
+
+    Raises:
+        RuntimeError: If the path does not exist or is not a directory.
+    """
+    if not path.exists():
+        logger.info(f"Error: Path '{path}' does not exist.")
+        path.mkdir(parents=True, exist_ok=True)
+        logger.info(f"Created directory: {path}")
+    logger.info(f"Verified path: {path} exists.")
